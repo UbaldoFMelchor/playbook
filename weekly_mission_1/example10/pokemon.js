5beba9465ac1aa65667d00e6c7648e32eebb8aa2
UbaldoFMelchor <99291040+UbaldoFMelchor@users.noreply.github.com>
2022-04-13 15:17:51 -0500

    Add files via upload
    añadidos ejercicios semana 1 via github

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,9 @@
+export default class pokemon {
+    constructor(name) {
+        this.name = name
+    }
+    sayHello(){
+        console.log(`Mi pokemon ${[this.name]} te saluda!!!`)
+    }
+      
+}
