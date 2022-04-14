9bc40d915dc17fb337cb4163a730cdecde51fc82
UbaldoFMelchor <99291040+UbaldoFMelchor@users.noreply.github.com>
2022-04-14 14:06:39 -0500

    Add files via upload
    añadidos ejercicios desde github 
    desde git marco error

---
+++ weekly_mission_2/examples0/examples0-2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Ubaldo",
+    age: 24
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
+  
