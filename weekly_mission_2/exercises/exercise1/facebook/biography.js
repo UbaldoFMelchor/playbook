24d21379bfeb48f0dd9ced0f1ac02f4ce4d23bbd
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-22 23:56:47 -0500

    creacion fb biography

---
+++ weekly_mission_2/exercises/exercise1/facebook/biography.js
@@ -0,0 +1,11 @@
+const biography = {
+    name: "Ubaldo F Melchor",
+    friends: 214,
+    presentation: "It takes a little darkness to enjoy the light...",
+    location: "xalapa",
+    createDate: "may 2010",
+    hoobys: 4,
+    pictures: 512,
+    post:3501,
+
+}
