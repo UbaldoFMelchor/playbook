6e9ba6f268a58a7ad2740053d67c72668183bb04
UbaldoFMelchor <99291040+UbaldoFMelchor@users.noreply.github.com>
2022-04-24 00:58:14 -0500

    agregados console.logs para mejorar legitibilidad

--- weekly_mission_2/exercises/exercise2/explorers.js
+++ weekly_mission_2/exercises/exercise2/explorers.js
@@ -59,16 +59,23 @@ const explorers = [
     }
    ]
 
+   console.log("Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
+   console.log("Imprime el stack de cada explorer, usa FOR EACH")
    explorers.forEach(exp => console.log(exp.name + " " + exp.stack))
 
+   console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
    const listStacks = explorers.map ((exp) => exp.stack)
    console.log(listStacks)
 
+   console.log("Obtén la lista de explorers que tengan en su stack 'js', usa FILTER (para validar un elemento en un lista se usa el método includes)")
    const stackJS = explorers.filter(exp => exp.stack.includes("js"))
    console.log(stackJS)
 
+   console.log("Busca el primer explorer que sea de la CDMX, usa FIND")
    const locationCDMX = explorers.find((exp) => exp.city == "CDMX")
    console.log("explorers que viven en CDMX: " +locationCDMX.name)
 
+   console.log("Obtén la suma de todos los exercises_completed, usa REDUCE")
    const totalExCom = explorers.reduce((acc, exp) => acc + exp.exercises_completed, 0)
    console.log("total de ejercicios completados: " + totalExCom)
+
