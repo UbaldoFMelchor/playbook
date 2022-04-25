848f3a9ec6e43d88bfc71e5e41162c4880f78e56
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-25 13:44:45 -0500

    agregado ub travel

---
+++ weekly_mission_2/exercises/exercise3/uber/travel.js
@@ -0,0 +1,11 @@
+class travel {
+    constructor(ride,location,destination) {
+        this.ride = ride
+        this.location = location
+        this.destination = destination
+    }
+}
+
+const myTravel = new travel("tsuru","xalapa","veracruz")
+
+console.log(myTravel)
